7371aed0fdf6213e71061fc43fd73d9286bb78f1
Sameer Mohamed <smrbasil4@gmail.com>
2019-04-15 22:29:12 +0530

    Split Data into Train and Test

--- decision_tree_sklearn.py
+++ decision_tree_sklearn.py
@@ -15,19 +15,35 @@ raw_data = pd.read_csv('./data/adult.data')
 print("Data Set Shape =", raw_data.shape)
 
 raw_data = format_data(raw_data)
+sample_count = len(raw_data)
+print("No. of Samples = ", sample_count)
+test_percentage = 0.1
+test_sample_count = test_percentage * sample_count
 
-X = raw_data.iloc[:, :14]
-Y = raw_data.iloc[:, 14:]
+test_sample_count = test_sample_count.__round__()
+train_sample_count = sample_count - test_sample_count
+print("test =", test_sample_count)
+print("train =", train_sample_count)
+
+print("Splitting Data")
+train_X = raw_data.iloc[test_sample_count:, :14]
+train_Y = raw_data.iloc[test_sample_count:, 14:]
+test_X = raw_data.iloc[:test_sample_count, :14]
+test_Y = raw_data.iloc[:test_sample_count, 14:]
+
+print("train_X = ", train_X.shape)
+print("train_Y = ", train_Y.shape)
+print("test_X = ", test_X.shape)
+print("test_Y = ", test_Y.shape)
 
 clf = tree.DecisionTreeClassifier()
-clf = clf.fit(X, Y)
-print("Trained Classifier\n")
+clf = clf.fit(train_X, train_Y)
+print("\nTrained Classifier\n")
 
 # Predicting Y using X
 print("Predicting Y using X")
 print("[0] = Person earns less than or equal to 50K per year")
 print("[1] = Person earns more than 50K per year\n")
-
 
 prediction_data_1 = [[21, 'Private', 77516, 'Bachelors', 13, 'Never-married', 'Prof-specialty', 'Unmarried', 'Other',
                       'Male', 2174, 0, 40, 'India']]
