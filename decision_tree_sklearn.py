4528cfb515af5788292aba641d4e4400549741d8
Sameer Mohamed <smrbasil4@gmail.com>
2019-04-28 16:13:54 +0530

    Implemented Checking Accuracy and Removed Unnecessary Log Messages

--- decision_tree_sklearn.py
+++ decision_tree_sklearn.py
@@ -3,6 +3,7 @@ from sklearn import tree
 from sklearn.preprocessing import LabelEncoder
 
 
+# Convert Strings to Numeric Values
 def format_data(data):
     for column in data.columns:
         if data[column].dtype == type(object):
@@ -11,56 +12,35 @@ def format_data(data):
     return data
 
 
+# Load Data
 raw_data = pd.read_csv('./data/adult.data')
 print("Data Set Shape =", raw_data.shape)
 
 raw_data = format_data(raw_data)
 sample_count = len(raw_data)
-print("No. of Samples = ", sample_count)
 test_percentage = 0.1
+print("Train : Test Ratio =", 1 - test_percentage, ":", test_percentage)
 test_sample_count = test_percentage * sample_count
 
 test_sample_count = test_sample_count.__round__()
 train_sample_count = sample_count - test_sample_count
-print("test =", test_sample_count)
-print("train =", train_sample_count)
 
-print("Splitting Data")
 train_X = raw_data.iloc[test_sample_count:, :14]
 train_Y = raw_data.iloc[test_sample_count:, 14:]
 test_X = raw_data.iloc[:test_sample_count, :14]
 test_Y = raw_data.iloc[:test_sample_count, 14:]
 
-print("train_X = ", train_X.shape)
-print("train_Y = ", train_Y.shape)
-print("test_X = ", test_X.shape)
-print("test_Y = ", test_Y.shape)
-
 clf = tree.DecisionTreeClassifier()
 clf = clf.fit(train_X, train_Y)
-print("\nTrained Classifier\n")
+print("Trained Classifier")
 
-# Predicting Y using X
-print("Predicting Y using X")
-print("[0] = Person earns less than or equal to 50K per year")
-print("[1] = Person earns more than 50K per year\n")
+# Check Accuracy
+print("Checking Accuracy")
+correct_count = 0
+for x in range(test_sample_count):
+    prediction = clf.predict(test_X.iloc[[x]])
+    if prediction[0] == test_Y.iat[x, 0]:
+        correct_count += 1
 
-prediction_data_1 = [[21, 'Private', 77516, 'Bachelors', 13, 'Never-married', 'Prof-specialty', 'Unmarried', 'Other',
-                      'Male', 2174, 0, 40, 'India']]
-prediction_data_2 = [
-    [40, 'Private', 121772, 'Assoc-voc', 11, 'Married-civ-spouse', 'Craft-repair', 'Husband', 'Asian-Pac-Islander',
-     'Male', 0, 0, 40, '?']]
-
-prediction_data_frame = pd.DataFrame(prediction_data_2)
-prediction_data_frame = format_data(prediction_data_frame)
-
-prediction = clf.predict(prediction_data_frame)
-
-print("Prediction =", prediction)
-
-prediction_data_frame = pd.DataFrame(prediction_data_1)
-prediction_data_frame = format_data(prediction_data_frame)
-
-prediction = clf.predict(prediction_data_frame)
-
-print("Prediction =", prediction)
+accuracy = (correct_count / test_sample_count) * 100
+print("Accuracy = ", str(accuracy.__round__()) + "%")
