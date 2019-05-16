6556c5d21fb9102fe4df34285db54466e02d7ce7
Sameer Mohamed <smrbasil4@gmail.com>
2019-05-16 22:42:12 +0530

    Tried Variations

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
 import numpy as np
 import pandas as pd
 from sklearn.preprocessing import LabelEncoder
+from sklearn.tree import DecisionTreeClassifier
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import AdaBoostClassifier
 from sklearn.metrics import accuracy_score
+from sklearn.metrics import classification_report
 from sklearn.model_selection import train_test_split
 
 # Load the data set
 df = pd.read_csv("./data/adult.csv")
 
-# Replace unknown values with null
+# Replace unknown values with Nan
 df = df.replace('?', np.NaN)
 
 # Drop samples which contain Nan data
@@ -23,7 +26,7 @@ df['marital.status'] = df['marital.status'].replace(['Widowed', 'Divorced', 'Sep
 df['marital.status'] = df['marital.status'].replace(
     ['Married-spouse-absent', 'Married-civ-spouse', 'Married-AF-spouse'], 'married')
 
-# Objects are mapped to numerical values
+# Get columns which contain object data
 categorical_df = df.select_dtypes(include=['object'])
 
 # Load the label encoder
@@ -31,7 +34,9 @@ enc = LabelEncoder()
 
 categorical_df = categorical_df.apply(enc.fit_transform)
 
+# Removes the object data columns from the data set
 df = df.drop(categorical_df.columns, axis=1)
+# Adds the removed columns with the encoded data
 df = pd.concat([df, categorical_df], axis=1)
 
 # Separate inputs and outputs in the data set
@@ -41,6 +46,19 @@ y = df['income']
 # Split the data set into training data set and testing data set
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=24)
 
+##---
+clf = DecisionTreeClassifier(random_state=24)
+clf.fit(X_train, y_train)
+
+# Get predictions for the test input data
+y_pred = clf.predict(X_test)
+
+accuracy = accuracy_score(y_test, y_pred)
+
+print("Decision Tree accuracy = ", accuracy * 100)
+# print(classification_report(y_test, y_pred))
+
+#---
 clf = RandomForestClassifier(n_estimators=100, random_state=24)
 clf.fit(X_train, y_train)
 
@@ -49,4 +67,51 @@ y_pred = clf.predict(X_test)
 
 accuracy = accuracy_score(y_test, y_pred)
 
-print("Random Forests accuracy", accuracy * 100)
+print(" n_estimators = 100, Random Forests accuracy = ", accuracy * 100)
+# print(classification_report(y_test, y_pred))
+##---
+clf = RandomForestClassifier(n_estimators=1000, random_state=24)
+clf.fit(X_train, y_train)
+
+# Get predictions for the test input data
+y_pred = clf.predict(X_test)
+
+accuracy = accuracy_score(y_test, y_pred)
+
+print(" n_estimators = 1000, Random Forests accuracy = ", accuracy * 100)
+# print(classification_report(y_test, y_pred))
+#-----
+clf = AdaBoostClassifier(n_estimators=100, random_state=24)
+clf.fit(X_train, y_train)
+
+# Get predictions for the test input data
+y_pred = clf.predict(X_test)
+
+accuracy = accuracy_score(y_test, y_pred)
+
+print("n_estimators=100, Ada Boost accuracy", accuracy * 100)
+# print(classification_report(y_test, y_pred))
+
+#-----
+clf = AdaBoostClassifier(n_estimators=1000, random_state=24)
+clf.fit(X_train, y_train)
+
+# Get predictions for the test input data
+y_pred = clf.predict(X_test)
+
+accuracy = accuracy_score(y_test, y_pred)
+
+print("n_estimators=1000, Ada Boost accuracy", accuracy * 100)
+# print(classification_report(y_test, y_pred))
+
+#-----
+clf = AdaBoostClassifier(n_estimators=10000, random_state=24)
+clf.fit(X_train, y_train)
+
+# Get predictions for the test input data
+y_pred = clf.predict(X_test)
+
+accuracy = accuracy_score(y_test, y_pred)
+
+print("n_estimators=10000, Ada Boost accuracy", accuracy * 100)
+# print(classification_report(y_test, y_pred))
